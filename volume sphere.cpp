1d68f3939beaddc19e503178d21c8f59cead022e
Aayush-Dev-2003 <aayushkumayan2103@gmail.com>
2024-12-13 11:33:17 +0530

    Add files via upload

---
+++ volume sphere.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//#include<maths.h>
+int main(){
+	int v,r;
+	float pie = 3.147;
+	printf("enter the value of r");
+	scanf("%d", &r);
+	v=4/3*pie*r*r*r;
+	printf("the volue of sphere is = %d", v);
+}
